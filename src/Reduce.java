dbd3724d71db760b0aca32b1513f0ae8baf716ea
Anietta Weckauff <aniettaweckauff@Aniettas-MacBook-Air-4.local>
2023-09-11 12:38:10 -0400

    new class for Task 5: Reduce

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Reduce {
+public class Reduce {
+
+    public static void main(String[] args) {
+        int numberOfSteps = calculateNumberOfSteps(100);
+        System.out.println(numberOfSteps);
+    }
+
+    private static int calculateNumberOfSteps(int n) {
+        int counter = 0;
+        while(n > 0){
+            if(n % 2 == 0){
+                n = n/2;
+            } else{
+                n = n - 1;
+            }
+            counter++;
+        }
+        return counter;
+    }
 }
